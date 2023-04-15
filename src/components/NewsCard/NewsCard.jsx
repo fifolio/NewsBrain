d026655960b3002c79333f379d206d93ceecfab6
Firas <firasdabbabi@gmail.com>
2023-04-15 22:23:41 +0100

    version 1.6: display the Articles in NewsCard.jsx

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,48 +1,78 @@
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography, } from '@mui/material';
 
-export default function NewsCard() {
+export default function NewsCard({ article, index }) {
+
+    const { description, publishedAt, source, title, url, urlToImage } = article;
+
+    const classes = {
+        media: {
+            height: 250,
+        },
+        border: {
+            border: 'solid',
+        },
+        fullHeightCard: {
+            height: '100%',
+        },
+        card: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            borderBottom: '10px solid white',
+        },
+        activeCard: {
+            borderBottom: '10px solid #22289a',
+        },
+        grid: {
+            display: 'flex',
+        },
+        details: {
+            display: 'flex',
+            justifyContent: 'space-between',
+            margin: '20px',
+        },
+        title: {
+            padding: '0 16px',
+        },
+        cardActions: {
+            padding: '0 16px 8px 16px',
+            display: 'flex',
+            justifyContent: 'space-between',
+        },
+    }
+
     return (
-        <div>
-            <h1>Art</h1>
-        </div>
+        <Card>
+            <CardActionArea>
+                <CardMedia sx={classes.media} image={urlToImage || 'https://logodix.com/logo/570946.png'} title={title}>
+                </CardMedia>
+                <CardMedia>
+                    <div sx={classes.details}>
+                        <Typography variant="body2" color="textSecondary" component="h2">
+                            {(new Date(publishedAt)).toDateString()}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="h2">
+                            {source.name}
+                        </Typography>
+                    </div>
+                    <Typography sx={classes.title} gutterBottom variant="h5">
+                        {title}
+                    </Typography>
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {description}
+                        </Typography>
+                    </CardContent>
+                </CardMedia>
+            </CardActionArea>
+            <CardActions sx={classes.cardActions}>
+                <Button size="small" color="primary">
+                    Learn More
+                </Button>
+                <Typography variant="h5" color="textSecondary">
+                    {index + 1}
+                </Typography>
+            </CardActions>
+        </Card >
     )
-
-    // return (
-    //     <Card>
-    //         <CardActionArea>
-    //             <CardMedia
-    //                 image={urlToImage || 'https://logodix.com/logo/570946.png'}>
-    //                 <div>
-    //                     <Typography variant="body2" color="textSecondary" component="h2">
-    //                         {(new Data(publishedAt)).toDataString()}
-    //                     </Typography>
-
-    //                     <Typography variant="body2" color="textSecondary" component="h2">
-    //                         {source.name}
-    //                     </Typography>
-    //                 </div>
-
-    //                 <Typography gutterBottom variant="h5">
-    //                     {title}
-    //                 </Typography>
-    //                 <CardContent>
-    //                     <Typography variant="body2" color="textSecondary" component="p">
-    //                         {description}
-    //                     </Typography>
-    //                 </CardContent>
-
-    //             </CardMedia>
-    //         </CardActionArea>
-
-    //         <CardActions>
-    //             <Button size="small" color="primary">
-    //                 Learn More
-    //             </Button>
-    //             <Typography variant="h5" color="textSecondary">
-    //                 {index + 1}
-    //             </Typography>
-    //         </CardActions>
-
-    //     </Card>
-    // )
 }
--- src/components/NewsCards/NewsCards.jsx
+++ src/components/NewsCards/NewsCards.jsx
@@ -6,28 +6,23 @@ export default function NewsCards({ theArticles }) {
 
 
     return (
-        <div>
-            {theArticles.map((article, i) => (
-                <NewsCard />
-            ))}
-        </div>
-        // <Grow in>
-        //     <Grid
-        //         container
-        //         className={styles.container}
-        //         alignItems="stretch"
-        //         spacing={3}>
+        <Grow in>
+            <Grid
+                container
+                className={styles.container}
+                alignItems="stretch"
+                spacing={3}
+            >
 
-        //         {articles.map((article, index) => (
+                {theArticles.map((article, index) => (
 
-        //             <Grid item sx={6} md={4} lg={3} style={{ display: 'flex' }}>
-        //                 <NewsCard article={article} index={index} />
-        //             </Grid>
+                    <Grid item sx={6} md={4} lg={3} style={{ display: 'flex' }}>
+                        <NewsCard article={article} index={index} />
+                    </Grid>
 
-        //         ))}
+                ))}
 
-        //     </Grid>
-
-        // </Grow>
+            </Grid>
+        </Grow>
     )
 }
