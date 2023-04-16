be01b0666d4d17a3f5966bed0eb03a6cdf125103
Firas <firasdabbabi@gmail.com>
2023-04-16 01:09:03 +0100

    version 1.7: move the classes of the styling from NewsCard.jsx to styles.jsx & change sx={} to style={} in the NewsCard.jsx components

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,53 +1,18 @@
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography, } from '@mui/material';
 
+import classes from './styles';
+
 export default function NewsCard({ article, index }) {
 
     const { description, publishedAt, source, title, url, urlToImage } = article;
 
-    const classes = {
-        media: {
-            height: 250,
-        },
-        border: {
-            border: 'solid',
-        },
-        fullHeightCard: {
-            height: '100%',
-        },
-        card: {
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-            borderBottom: '10px solid white',
-        },
-        activeCard: {
-            borderBottom: '10px solid #22289a',
-        },
-        grid: {
-            display: 'flex',
-        },
-        details: {
-            display: 'flex',
-            justifyContent: 'space-between',
-            margin: '20px',
-        },
-        title: {
-            padding: '0 16px',
-        },
-        cardActions: {
-            padding: '0 16px 8px 16px',
-            display: 'flex',
-            justifyContent: 'space-between',
-        },
-    }
-
     return (
-        <Card>
-            <CardActionArea>
-                <CardMedia sx={classes.media} image={urlToImage || 'https://logodix.com/logo/570946.png'} title={title}>
+        <Card style={classes.card}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia style={classes.media} image={urlToImage || 'https://logodix.com/logo/570946.png'} title={title}>
                 </CardMedia>
                 <CardMedia>
-                    <div sx={classes.details}>
+                    <div style={classes.details}>
                         <Typography variant="body2" color="textSecondary" component="h2">
                             {(new Date(publishedAt)).toDateString()}
                         </Typography>
@@ -55,7 +20,7 @@ export default function NewsCard({ article, index }) {
                             {source.name}
                         </Typography>
                     </div>
-                    <Typography sx={classes.title} gutterBottom variant="h5">
+                    <Typography style={classes.title} gutterBottom variant="h5">
                         {title}
                     </Typography>
                     <CardContent>
@@ -65,7 +30,7 @@ export default function NewsCard({ article, index }) {
                     </CardContent>
                 </CardMedia>
             </CardActionArea>
-            <CardActions sx={classes.cardActions}>
+            <CardActions style={classes.cardActions}>
                 <Button size="small" color="primary">
                     Learn More
                 </Button>
