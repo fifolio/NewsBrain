a342c9f6a0e230dbfce590acff4a90ff0556c7f7
Firas <firasdabbabi@gmail.com>
2023-04-19 16:03:48 +0100

    version 2.6: Add scrolling while reading Headlines function to NewsCard.jsx

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, createRef } from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography, } from '@mui/material';
 import classes from './styles';
 import activeCard from './activeCard';
@@ -15,8 +16,37 @@ export default function NewsCard({ article, index, activeArticle }) {
     } = article;
 
 
+    // Create Element References state
+    const [elRefs, setElRefs] = useState([]);
+
+    const scrollToRef = ref => window.scroll(0, ref.current.offsetTop - 100);
+
+    useEffect(() => {
+        // the refs argument is the current value of the elRefs state variable.
+        setElRefs((refs) =>
+            //  using the Array method to create a new array with 20 elements
+            Array(20)
+                // The fill() method is used to fill all of the elements with undefined, since we don't need any specific values for this array.
+                .fill()
+                // The map method is used to iterate over each element in the array and create a new array of React refs.
+                .map(
+                    //  The _ parameter in the arrow function is a placeholder variable for the current element in the array that we don't need to use, and the index parameter is the index of the current element.
+                    (_, index) =>
+                        // Inside the arrow function, we're checking if a ref already exists at the current index in the refs array (which is passed as an argument to setElRefs). If a ref exists, we use it. If not, we create a new ref using the createRef function.
+                        refs[index] || createRef())
+            // Finally, we pass this new array of refs to the setElRefs function, which updates the elRefs state variable with the new array.
+        );
+    }, [])
+
+    useEffect(() => {
+        if (index === activeArticle && elRefs[activeArticle]) {
+            scrollToRef(elRefs[activeArticle]);
+        }
+    }, [index, activeArticle, elRefs])
+
     return (
         <Card
+            ref={elRefs[index]}
             style={activeArticle === index ? activeCard.card : classes.card}>
             <CardActionArea href={url} target="_blank">
                 <CardMedia style={classes.media} image={urlToImage || 'https://logodix.com/logo/570946.png'} title={title}>
