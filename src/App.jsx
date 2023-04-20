615cab562bdb5bc1442663768ebf4b265d636393
Firas <firasdabbabi@gmail.com>
2023-04-20 14:07:48 +0100

    version 3: touch ScrollUp.jsx and Customize the Btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState, useEffect } from 'react'
-import alanBtn from '@alan-ai/alan-sdk-web'
+import { useState, useEffect } from 'react';
+// import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
 const alanKey = 'c5a66bb5abc2aa65c4db81d3452c51ab2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 import Navbar from './components/Navbar/Navbar'
 import NewsCards from './components/NewsCards/NewsCards';
 import Footer from './components/Footer/Footer'
 import HeroSection from './components/HeroSection/HeroSection';
+import ScrollUp from './components/Btns/ScrollUp';
 
 export default function App() {
   const [newsArticles, setNewsArticles] = useState([]);
   const [activeArticle, setActiveArticle] = useState(-1)
 
-
-
-
-  useEffect(() => {
+    useEffect(() => {
     console.clear();
     alanBtn({
       key: alanKey,
@@ -38,11 +37,11 @@ export default function App() {
   }, []);
 
 
-
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <ScrollUp />
       <NewsCards theArticles={newsArticles} activeArticle={activeArticle} />
       <Footer />
     </div>
---
+++ src/components/Btns/ScrollUp.jsx
@@ -0,0 +1,52 @@
+import { Button } from '@mui/material';
+import { useState, useEffect } from 'react';
+
+export default function ScrollUp() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        // cleanup function to remove the event listener when the component unmounts
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const handleScroll = () => {
+        if (window.pageYOffset > 20) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
+    const buttonStyle = {
+        position: "fixed",
+        bottom: "45px",
+        left: "75px",
+        transform: "translateX(-50%)",
+        padding: "10px",
+        background: 'black',
+        color: 'white',
+        fontWeight: '500',
+        outline: "none",
+        textTransform: 'capitalize',
+        display: isVisible ? "block" : "none",
+        zIndex: 1,
+    };
+
+    return (
+        <>
+            <Button size="small" variant="outlined" style={buttonStyle} onClick={scrollToTop} >
+                Return Up
+            </Button >
+        </>
+    );
+}
