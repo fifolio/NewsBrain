b0bb8f92801037da122e9db5b5137adccd2d21ff
FIRAS <97522852+fifolio@users.noreply.github.com>
2023-04-30 17:37:03 +0100

    Update brain.js
    Adding new features to the Brain

--- brain.js
+++ brain.js
@@ -1,5 +1,21 @@
-const API_KEY = '9e16f4adeeea43fbb80f8684be2986e6';
+const API_KEY = 'ADD_YOU_API_KEY_HERE';
 let savedArticles = [];
+
+intent('What is your name', (p) => {
+p.play("As an AI language model, I don't have a name, but you can call me Brain!")
+})
+
+intent('(Where are you from | Where are you from Brain)', (p) => {
+p.play("I was created by fifolio 'who developed me', so you could say I'm from the digital world!")
+})
+
+intent('(What do you do for work or study | What do you do)', (p) => {
+p.play(" my 'work' is to assist and communicate with humans. I was trained on a diverse range to improve my Brain skills.")
+})
+
+intent('What are your (hobbies | interests)', (p) => {
+p.play("I don't have any hobbies or interests like humans do, but I enjoy learning new things and helping people with their questions and tasks.")
+})
 
 // News by source
 intent(`(Give me | tell me | show me) the news (from | by | on | in) $(source* .+)`, (p) => {
@@ -17,7 +33,7 @@ intent(`(Give me | tell me | show me) the news (from | by | on | in) $(source* .
         }
         
       savedArticles = articles;
-        
+        p.play(`Hold on for a moment and I will provide you with the $(source) articles you asked for..`)
         p.play({ command: 'newHeadlines', articles });
         p.play(`Here are the (latest|recent) (from | by | on | in) ${p.source.value}`);
         
@@ -42,7 +58,7 @@ intent(`What's up with $(term* .+)`, (p) => {
         }
         
       savedArticles = articles;
-        
+        p.play('Please give me a moment and I will provide you with the articles you asking for..')
         p.play({ command: 'newHeadlines', articles });
         p.play(`Here are the (latest|recent) articles (from | by | on | in) ${p.term.value}`);
         
@@ -70,7 +86,7 @@ intent(`show me $(categories~ ${categories_intent}) (news|headlines)`, (p) => {
         }
         
       savedArticles = articles;
-        
+         p.play('Sure, please give me a sec. and I will provide you with the articles you asking for..')
         p.play({ command: 'newHeadlines', articles });
         if(p.categories.value) {
          p.play(`Here are the (latest|recent) posts (from | by | on | in) ${p.categories.value}`);
@@ -84,7 +100,7 @@ intent(`show me $(categories~ ${categories_intent}) (news|headlines)`, (p) => {
 });
 
 // Latest|recent news
-intent(`Give me the (latest|recent) news`, (p) => {
+intent(`(Give me | tell me | show me) the (latest|recent) news`, (p) => {
     let NEWS_API_URL = `https://newsapi.org/v2/top-headlines?apiKey=${API_KEY}&country=us`;
            
     api.axios.get(NEWS_API_URL).then((response)=>{
@@ -96,6 +112,7 @@ intent(`Give me the (latest|recent) news`, (p) => {
         
       savedArticles = articles;
      
+         p.play('Please give me a moment so I provide you with the articles you asking for..')
         p.play({ command: 'newHeadlines', articles });
         if(articles) {
          p.play(`Here are the (latest|recent) news`);
@@ -103,11 +120,12 @@ intent(`Give me the (latest|recent) news`, (p) => {
         
         p.play(`Would you like me to read the headlines?`);
         p.then(confirmation);
-    });
-});
+    })
+})
 
 const confirmation = context(() => {
     intent(`(Yes|Sure|Yes Please|Please|Go Ahead|Do it|Why not)`, async (p) => {
+        p.play(`Alright!`)
         for(let i = 0; i < savedArticles.length; i++){
             p.play({command: 'highlight', article: savedArticles[i]});
             p.play(`${savedArticles[i].title}`)
@@ -118,3 +136,18 @@ const confirmation = context(() => {
         p.play(`Sure! sounds good to me.`)
     })
 })
+
+intent(`open (the|) (article|) (number|) $(number* (.*))`, (p) => {
+    
+    if(p.number.value) {
+    p.play(`Okay! here's article ${p.number.value}`) 
+    p.play({ command:'open', number: p.number.value, articles: savedArticles})
+}
+})
+
+
+intent(`go back`, (p) => {
+    p.play('Sure, going back');
+    p.play({command: 'newHeadlines', articles: []})
+})
+
